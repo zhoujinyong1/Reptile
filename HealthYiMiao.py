f38961f1f071fc2b93ece502a285c873f742e81d
zhoujinyong1 <51100742+zhoujinyong1@users.noreply.github.com>
2021-06-03 17:21:30 +0800

    Update HealthYiMiao.py

--- HealthYiMiao.py
+++ HealthYiMiao.py
@@ -5,11 +5,11 @@ import time
 import ctypes
 from urllib.parse import parse_qs, urlencode, urlparse
 
-#获取健康深圳页面
+#获取健康深圳页面(具体根据抓包信息自行更换，有些信息涉及本人就用信息代替)
 reservation_headers = {
     "Accept-Encoding" :"gzip, deflate, br",
     "User-Agent" :"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
-    "Cookie" :"xkypsession=MTZmZTEyMjktODgzZC00NmEwLTkzMTMtODNkMzkzMDUxZjI3",
+    "Cookie" :"信息",
     "Content-Length" :"247",
     "content-type" :"application/x-www-form-urlencoded;charset=UTF-8",
     "Referer" :"https://servicewechat.com/wx430d371fb280bcd5/114/page-frame.html",
@@ -17,15 +17,15 @@ reservation_headers = {
     "Host" :"www.newhealth.com.cn"
 }
 reservation_data = {
-  "platOrgId" :"440311007004",
-  "platDeptId" :"210407207C160FE9",
+  "platOrgId" :"信息",
+  "platDeptId" :"信息",
   "schDate" :"",
-  "channelId" :"31",
+  "channelId" :"信息",
   "rn" :"8056871556673852",
   "rtime" :"20210603111735",
   "sko" :"d67373293b966a60a362f5cb01b7610b",
-  "_wx_url" :"shekang%252Fpages%252Fdepartment%252Fmain",
-  "_wx_mini_requestId" :"xky202106032985793054150152"
+  "_wx_url" :"信息",
+  "_wx_mini_requestId" :"信息"
 }
 reservation_url = 'https://www.newhealth.com.cn/hos/portal/appt/getDoctorSchInfo'
 fast_access_url_is = requests.post(reservation_url,data=reservation_data,headers=reservation_headers).json()['body']['dataList']
